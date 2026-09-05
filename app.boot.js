de8e1f0200fc7032207a1d9f768a08d84e3e26f9
nenae16-creator <nenae16@gmail.com>
2026-09-05 16:36:30 +0900

    Add meet data parts and chained boot

--- app.boot.js
+++ app.boot.js
@@ -1,4 +1,7 @@
 (async function(){
+  const da = await (await fetch("data.p1.txt")).text();
+  const db = await (await fetch("data.p2.txt")).text();
+  eval(da+db);
   const a = await (await fetch("app.p1.txt")).text();
   const b = await (await fetch("app.p2.txt")).text();
   eval(decodeURIComponent(escape(atob(a+b))));
